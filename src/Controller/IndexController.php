ea39e180ce88006ee4c938fdd1a83ae4c6ee2910
Rodrigo Battagliero <rbattagliero@gmail.com>
2026-05-01 19:42:40 -0300

    Create base controller

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/api/init', name: 'app_init', methods: ['POST'])]
+    public function init(): JsonResponse
+    {
+        
+        return $this->json(null, 201);
+    }
+
+    #[Route('/api/add', name: 'app_add_product', methods: ['POST'])]
+    public function addProduct(): JsonResponse
+    {
+
+        return $this->json(null, 201);
+    }
+
+    #[Route('/api/total', name: 'app_total', methods: ['GET'])]
+    public function getTotal(): JsonResponse
+    {
+
+        return $this->json(['total' => 1]);
+    }
+}
